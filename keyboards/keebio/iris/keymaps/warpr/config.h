822b0b326b41f2b8d9828fc042af0960acd7d48b
Kuno Woudt <kuno@frob.nl>
2021-07-09 06:40:44 -0500

    Remove warp_mods for now

--- keyboards/keebio/iris/keymaps/warpr/config.h
+++ keyboards/keebio/iris/keymaps/warpr/config.h
@@ -20,5 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 
+#define TAPPING_TOGGLE 1
 #define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
