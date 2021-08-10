c7245f32718b2771aa3feeaa340da1cbd6c219d4
Kuno Woudt <kuno@frob.nl>
2021-08-09 22:46:46 -0500

    add encoder_mouse.c (currently broken)

--- keyboards/keebio/iris/keymaps/warpr/rules.mk
+++ keyboards/keebio/iris/keymaps/warpr/rules.mk
@@ -18,6 +18,7 @@ BOOTLOADER = qmk-dfu
 
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
+POINTING_DEVICE_ENABLE = yes
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -33,3 +34,5 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 SPLIT_KEYBOARD = yes
 ENCODER_ENABLE = yes
+
+SRC += "encoder_mouse.c"
