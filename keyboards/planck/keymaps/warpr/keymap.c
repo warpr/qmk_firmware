4eca5dbe9216f98f51c82d1d001811ea5e622289
Kuno Woudt <kuno@frob.nl>
2021-08-17 22:31:03 -0500

    HOLD and DROP implementation with LT() style hold/tap

--- keyboards/planck/keymaps/warpr/keymap.c
+++ keyboards/planck/keymaps/warpr/keymap.c
@@ -25,10 +25,15 @@
 #define _DVORAK 0
 #define _SYM 1
 #define _NAV 2
-#define _SYSTEM 3
+#define _MOD_L 3
+#define _MOD_R 4
+#define _SYSTEM 5
 
 enum custom_keycodes {
-  WORD_L = SAFE_RANGE,  // emacs word left (ESC, B)
+  HOLD_MIN = (SAFE_RANGE | 0x0f) + 1,
+  HOLD_MAX = HOLD_MIN + 0x10,
+  DROP,
+  WORD_L,  // emacs word left (ESC, B)
   WORD_R,  // emacs word right (ESC, F)
   PAGE_B,  // emacs backward-page, C-X [
   PAGE_F,  // emacs forward-page, C-X ]
@@ -42,6 +47,10 @@ enum custom_keycodes {
 #define KW_ENTER   LT(_SYM, KC_ENT)
 #define KW_NAV     MO(_NAV)
 
+#define HOLD(layer) (HOLD_MIN | ((layer)&0x0F))
+#define HOLD_SPC   LT(_MOD_L, KC_SPC)
+#define HOLD_ENT   LT(_MOD_R, KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = LAYOUT_planck_grid(
@@ -52,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSPO, KC_SCOLON, KC_Q,  KC_J,    KC_K,    KC_X,                               KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KW_FRSH, _______, KC_LOPT, KC_LCMD, KW_SPACE, KW_SPACE,                         KW_SPACE, KW_ENTER, KW_NAV, KW_NAV,  _______, KW_DEVT
+     KW_FRSH, _______, KC_LOPT, KC_LCMD, HOLD(_MOD_L), HOLD_SPC,                    HOLD_SPC, HOLD_ENT, HOLD(_MOD_R), KW_NAV, KW_EMOJ, KW_DEVT
   //└────────┴────────┴────────┴────────┴────────┴────────┘                          └────────┴────────┴────────┴────────┴────────┴────────┘
   ),
 
@@ -80,6 +89,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //└────────┴────────┴────────┴────────┴────────┴────────┘                          └────────┴────────┴────────┴────────┴────────┴────────┘
   ),
 
+  [_MOD_L] = LAYOUT_planck_grid(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     DROP,    DROP,   TO(_NAV), TO(_SYM), DROP,   DROP,                               DROP,    DROP,    DROP,    DROP,    DROP,    DROP,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     DROP,    KC_LOPT, KC_LCMD, KC_LSFT, KC_LCTL, DROP,                               DROP,    DROP,    DROP,    DROP,    DROP,    DROP,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     DROP,    DROP,    DROP,    DROP,    DROP,    DROP,                               DROP,    DROP,    DROP,    DROP,    DROP,    DROP,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     DROP,    DROP,    DROP,    DROP,    _______, DROP,                               DROP,    DROP,    _______, DROP,    DROP,    DROP
+  //└────────┴────────┴────────┴────────┴────────┴────────┘                          └────────┴────────┴────────┴────────┴────────┴────────┘
+  ),
+
+  [_MOD_R] = LAYOUT_planck_grid(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     DROP,    DROP,    DROP,    DROP,    DROP,    DROP,                               DROP,    DROP,   TO(_SYM), TO(_NAV), DROP,   DROP,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     DROP,    DROP,    DROP,    DROP,    DROP,    DROP,                               DROP,    KC_RCTL, KC_RSFT, KC_RCMD, KC_ROPT, DROP,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     DROP,    DROP,    DROP,    DROP,    DROP,    DROP,                               DROP,    DROP,    DROP,    DROP,    DROP,    DROP,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     DROP,    DROP,    DROP,    DROP,    _______, DROP,                               DROP,    DROP,    _______, DROP,    DROP,    DROP
+  //└────────┴────────┴────────┴────────┴────────┴────────┘                          └────────┴────────┴────────┴────────┴────────┴────────┘
+  ),
+
   [_SYSTEM] = LAYOUT_planck_grid(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                            KC_INS,  KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,
@@ -93,6 +126,83 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 
 };
+
+static uint8_t warp_hold_active = 0;
+
+bool process_drop(uint16_t keycode, keyrecord_t *record) {
+    uint8_t layer = 0;
+
+    switch (keycode) {
+    case DROP:
+        if (record->event.pressed) {
+            for (int8_t i = MAX_LAYER - 1; i >= 0; i--) {
+                if (layer_state_is(i)) {
+                    if (DROP == keymap_key_to_keycode(i, record->event.key)) {
+                        // turn off this layer, and continue dropping down.
+                        layer_off(i);
+                    } else {
+                        // no DROP at this key pos in the current layer, we're done.
+                        return true;
+                    }
+                }
+            }
+
+            // we've dropped all the way down to 0 and still found a DROP keycode there,
+            // that doesn't seem a useful configuration.  Let's just claim we handled it.
+            return false;
+        }
+
+        return false;
+
+    case HOLD_SPC:
+    case HOLD_ENT:
+        if (record->tap.count) {
+            // regular key press, leave this to QMK
+            return true;
+        }
+
+        layer = (keycode >> 8) & 0x0F;
+        uprintf("HOLD_SPC/HOLD_ENT is active. Switching to layer 0x%02X.  keycode is 0x%04X\n", layer, keycode);
+
+        // fall through to HOLD key handling
+
+    case HOLD_MIN ... HOLD_MAX:
+        if (record->event.pressed) {
+            if (!layer) {
+                layer = keycode & 0x07;
+            }
+            layer_on(layer);
+            uprintf("HOLD is active. Switching to layer 0x%02X.  keycode is 0x%04.  Mods are 0x%02X\n", layer, keycode, get_mods());
+            warp_hold_active = 1;
+            return false;
+        } else {
+            warp_hold_active = 0;
+            layer_clear();
+            uint8_t current_mods = get_mods();
+            clear_mods();
+            unregister_mods(current_mods);
+            uprintf("HOLD released.  Mods are 0x%02X\n", get_mods());
+            return false;
+        }
+        break;
+
+    case KC_LCTRL ... KC_RGUI:
+        if (record->event.pressed) {
+            return true;
+        } else {
+            if (warp_hold_active) {
+                uprintf("HOLD active, not releasing mods: 0x%02X\n", get_mods());
+                // don't release mods while hold is active, just claim we handled it
+                return false;
+            } else {
+                return true;
+            }
+        }
+        break;
+    }
+
+    return true;
+}
 
 bool process_emacs_nav(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -168,10 +278,24 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
     },
 
     [_NAV] = {
-        KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,          KW_WHITE, KW_WHITE, KW_PURPL, KW_WHITE, KW_WHITE, KW_WHITE,
+        KW_BLACK, KW_RED,   KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,          KW_WHITE, KW_WHITE, KW_PURPL, KW_WHITE, KW_WHITE, KW_WHITE,
         KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,          KW_WHITE, KW_PURPL, KW_PURPL, KW_PURPL, KW_WHITE, KW_WHITE,
         KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,          KW_BLACK, KW_RED,   KW_RED,   KW_BLACK, KW_BLACK, KW_BLACK,
         KW_BLACK, KW_BLACK, KW_GREEN, KW_GREEN, KW_BLACK,          KW_BLACK,           KW_BLACK, KW_GREEN, KW_GREEN,  KW_BLACK, KW_BLACK
+    },
+
+    [_MOD_L] = {
+        KW_BLACK, KW_BLACK, KW_GREEN, KW_GREEN, KW_BLACK, KW_BLACK,          KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,
+        KW_BLACK, KW_GREEN, KW_GREEN, KW_GREEN, KW_GREEN, KW_BLACK,          KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,
+        KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,          KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,
+        KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,           KW_BLACK,          KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK
+    },
+
+    [_MOD_R] = {
+        KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,          KW_BLACK, KW_BLACK, KW_GREEN, KW_GREEN, KW_BLACK, KW_BLACK,
+        KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,          KW_BLACK, KW_GREEN, KW_GREEN, KW_GREEN, KW_GREEN, KW_BLACK,
+        KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,          KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,
+        KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,           KW_BLACK,          KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK
     },
 
     [_SYSTEM] = {
@@ -180,6 +304,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
         KW_BLACK, KW_BLUE,  KW_BLUE,  KW_BLUE,  KW_BLUE,  KW_BLACK,          KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_RED,
         KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK,           KW_BLACK,          KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK, KW_BLACK
     },
+
 };
 
 void set_layer_color(int layer) {
@@ -196,27 +321,48 @@ void set_layer_color(int layer) {
 }
 
 void rgb_matrix_indicators_user(void) {
-  if (rgb_matrix_get_suspend_state() || keyboard_config.disable_layer_led) { return; }
-  switch (biton32(layer_state)) {
-    case 0:
-      set_layer_color(0);
+  if (rgb_matrix_get_suspend_state() || keyboard_config.disable_layer_led) {
+      return;
+  }
+
+  uint8_t layer = biton32(layer_state);
+  switch (layer) {
+  case 0 ... 5:
+      set_layer_color(layer);
       break;
-    case 1:
-      set_layer_color(1);
+  default:
+      if (rgb_matrix_get_flags() == LED_FLAG_NONE) {
+          rgb_matrix_set_color_all(0, 0, 0);
+      }
       break;
-    case 2:
-      set_layer_color(2);
-      break;
-    case 3:
-      set_layer_color(3);
-      break;
-   default:
-    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
-      rgb_matrix_set_color_all(0, 0, 0);
-    break;
   }
 }
 
+/*
+- tmk_core/common/action.c::process_record(record)
+  |
+  +-- quantum/quantum.c::process_record_quantum(record)
+  |   |
+  |   + -- process_record_kb(keycode, record)
+  |   |    |
+  |   |    +-- process_record_user(keycode, record)
+  |   |
+  |   +-- process_action_kb(record)
+  |
+  +-- process_record_handler(record)
+  |   |
+  |   +-- store_or_get_action(record->event.pressed, record->event.key)
+  |   |   |
+  |   |   +-- layer_switch_get_action(key)
+  |   |
+  |   +-- process_action(record, action)
+  |
+  +-- post_process_record_quantum(record)
+*/
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    return process_emacs_nav(keycode, record);
+    return (
+        process_drop(keycode, record) &&
+        process_emacs_nav(keycode, record)
+    );
 }
